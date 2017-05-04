317eac8eb9d395b692fcf5c1720dd23fcc986703
ercas <edgarrimandocastro@gmail.com>
2017-05-04 16:31:23 -0400

    polygon subtraction using shapely

--- osm_diff.py
+++ osm_diff.py
@@ -1,70 +1,89 @@
 #!/usr/bin/env python3
 
 from lxml import etree
+import copy
 import os
+import shapely.geometry
 
-def subtract(main_osm, subtract_osm, output_file = None):
+def blank_way(way):
+    new = etree.Element("way")
+    for item in way.items():
+        new.set(item[0], item[1])
+    for child in way.getchildren():
+        if (child.tag != "nd"):
+            way.append(child)
+    return new
+
+def subtract(osm, poly, output_file = None):
     """ Subtract one OSM XML file's contents from another OSM XML file
 
     Args:
-        main_osm: The path to the main OSM XML file.
-        subtract_osm: The path to the OSM XML file to be subtracted from
-            main_osm.
+        osm: The path to the main OSM XML file.
+        subtract_osm: The polygon to be subtracted from osm.
         output_file: The path to write the difference OSM XML to.
     """
-    main = etree.parse(main_osm)
-    mainroot = main.getroot()
-    subtract = etree.parse(subtract_osm)
+    tree = etree.parse(osm)
+    root = tree.getroot()
+    unique_id = -1
 
     remove = {}
-    orig_num = main.xpath("count(//*)")
+    orig_num = tree.xpath("count(//*)")
 
-    for element in subtract.getroot().getchildren():
-        if ("id" in element.keys()):
-            remove[element.get("id")] = element.tag
-    for element in mainroot.getchildren():
-        if ("id" in element.keys()):
+    # Remove nodes inside the polygon
+    for node in root.findall(".//node"):
+        if (poly.contains(shapely.geometry.Point(
+            float(node.get("lon")),
+            float(node.get("lat"))
+        ))):
+            remove[node.get("id")] = node.tag
+            root.remove(node)
 
-            # Remove elements with intersecting IDs
-            _id = element.get("id")
-            if (_id in remove):
-                if (element.tag == remove[_id]):
-                    mainroot.remove(element)
+    # For ways: remove references to deleted nodes
+    for way in root.findall(".//way"):
+        if (int(way.get("id")) > 0):
+            removing = False
+            appending = []
 
-            # For all ways: remove nodes that were previously removed
-            elif (element.tag == "way"):
-                for node in element.getchildren():
-                    if (node.tag == "nd"):
-                        ref = node.get("ref")
-                        if (ref in remove):
-                            element.remove(node)
+            nodes = way.findall(".//nd")
+            new_way = blank_way(way)
+
+            # Remove ways with no nodes
+            if (nodes == 0):
+                root.remove(way)
+
+            # Attempt to split other ways if possible
+            # TODO: fix
 
     if (output_file is None):
-        main.write("%s-%s" % (main_osm, subtract_osm))
+        tree.write("%s-poly.osm" % osm.rstrip(".osm"))
     else:
-        main.write(output_file)
+        tree.write(output_file)
 
-def subtract_multi(main_osm, subtract_osm_array, output_file = None):
+    print("Removed %d elements" % (orig_num - tree.xpath("count(//*)")))
+
+def subtract_multi(osm, polygons, output_file = None):
     """ Wraper for multiple subtractions
 
     Args:
-        main_osm: The path to the main OSM XML file.
-        subtract_osm_array: An array of paths to OSM XML files to be subtracted
-            from the main OSM XML file, in order of subtraction.
+        osm: The path to the OSM XML file.
+        polygons: An array of shapely polygons to be subtracted from osm.
         output_file: The path to write the difference OSM XML to.
     """
+
     tempfile = None
-    for subtract_osm in subtract_osm_array:
+
+    for polygon in polygons:
         if (tempfile is None):
-            tempfile = "temp%d.osm" % hash(main_osm)
-            subtract(main_osm, subtract_osm, tempfile)
+            tempfile = "temp%d.osm" % hash(osm)
+            subtract(osm, polygon, tempfile)
         else:
-            subtract(tempfile, subtract_osm, tempfile)
+            subtract(tempfile, polygon, tempfile)
+
     if (output_file is None):
-        os.rename(tempfile, "%s-%s" % (main_osm, "-".join(subtract_osm_array)))
+        os.rename(tempfile, "%s-poly-multi.osm" % osm.rstrip(".osm"))
     else:
         os.renamve(tempfile, output_file)
 
 if (__name__ == "__main__"):
-    subtract("map.osm", "map_sub.osm")
-    subtract_multi("map.osm", ["map_sub.osm", "map_sub2.osm"])
+    poly = shapely.geometry.box(-70.8983396, 42.5174601, -70.8937657, 42.5201496)
+    subtract("map.osm", poly)
